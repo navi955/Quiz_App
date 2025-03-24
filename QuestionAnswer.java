814f94872b571639be72ba1a314ec36dde9e856e
navi955 <nr1851159@gmail.com>
2025-03-24 12:47:43 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question [] = {
+            "What is 10 + 26 ? ",
+            "Who invented Telephone?",
+            "What is 12*9 ?",
+            "Who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?"
+    };
+
+    public static String choices [][] = {
+            {"32", "42", "36", "38"},
+            {"Graham Bell", "Einstein", "Edison", "None of the above"},
+            {"96", "84", "102", "108"},
+            {"jeff Bezos", "Elon Musk", "Steve jobs", "Bill Gates"},
+            {"Windows", "Linux", "Macos", "All  of the above"},
+    };
+
+    public static String correctAnswers []={
+            "36",
+            "Gramham Bell",
+            "108",
+            "Elon Musk",
+            "All of the above",
+    };
+}
